c8268a6c3cb24de5527a8d612174b03b32db80fd
Vinay Aggarwal <aryans.vinay@gmail.com>
2013-09-17 00:03:54 +0530

    Base URL updated

--- public/js/defaults.js
+++ public/js/defaults.js
@@ -3,25 +3,17 @@
 	Default Settings of IDE
 */
 
-
-(function() {
-  define(function() {
-    var defaults;
-    defaults = {
-      debug: true,
-      server: 'http://cup-code/',
-      theme: 'ace/theme/dawn',
-      mode: 'ace/mode/php',
-      fontSize: '14px',
-      emmet: true,
-      love: true,
-      jas: true
-    };
-    return window.Settings = defaults;
-  });
-
-}).call(this);
-
-/*
-//@ sourceMappingURL=defaults.map
-*/
+define(function() {
+  var defaults;
+  defaults = {
+    debug: true,
+    server: window.location.href.match("heroku") ? "http://cup-code.herokuapp.com/public/" : 'http://cup-code/',
+    theme: 'ace/theme/dawn',
+    mode: 'ace/mode/php',
+    fontSize: '14px',
+    emmet: true,
+    love: true,
+    jas: true
+  };
+  return window.Settings = defaults;
+});
