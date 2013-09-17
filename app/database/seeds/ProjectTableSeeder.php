fbd863c1972616257664289154b0a10f71e2865c
Vinay Aggarwal <aryans.vinay@gmail.com>
2013-09-18 00:50:00 +0530

    Shamefull Situation: Forgot to add ')' :(

--- app/database/seeds/ProjectTableSeeder.php
+++ app/database/seeds/ProjectTableSeeder.php
@@ -10,7 +10,7 @@ class ProjectTableSeeder extends Seeder
 	{
 		$posts = array(
 			array('hash_id'=> '5205dd4740cde', 'title'=>'My Awesome Project', 'private_path'=> '/app/','public_path'=>'/www/','created_at'=> date('Y-m-d H:i:s'),'updated_at'=> date('Y-m-d H:i:s')),
-			array('hash_id'=> '5205dd4740cde', 'title'=>'My Another Awesome Project', 'private_path'=> '/app/','public_path'=>'/www/','created_at'=> date('Y-m-d H:i:s'),'updated_at'=> date('Y-m-d H:i:s')
+			array('hash_id'=> '5205dd4740cde', 'title'=>'My Another Awesome Project', 'private_path'=> '/app/','public_path'=>'/www/','created_at'=> date('Y-m-d H:i:s'),'updated_at'=> date('Y-m-d H:i:s'))
 		);
 
 		DB::table('projects')->insert($posts);
